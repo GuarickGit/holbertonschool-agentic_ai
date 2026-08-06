be1eaf5198b5cee22d38c541382a352d34e83922
GuarickGit <10615@holbertonstudents.com>
2026-08-06 15:33:31 +0200

    Ajout de Z-50 pour forcer le header à toujours être au dessus

--- front_end-frameworks/react/src/components/Header.jsx
+++ front_end-frameworks/react/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { BrainCircuit } from "lucide-react";
 
 function Header() {
   return (
-    <header className="fixed top-0 right-0 left-0 bg-slate-950">
+    <header className="fixed top-0 right-0 left-0 z-50 bg-slate-950">
       <div className="flex items-center justify-between px-4 py-4 md:px-8 lg:px-20">
         {/* Logo + Titre */}
         <a className="flex items-center gap-2" href="#">
