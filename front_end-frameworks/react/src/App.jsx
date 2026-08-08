2bda0cac2fe250cdf7ac89ac39a172ca60f2102e
GuarickGit <10615@holbertonstudents.com>
2026-08-08 15:42:03 +0200

    Ajout de la balise Main pour l'access et le seo

--- front_end-frameworks/react/src/App.jsx
+++ front_end-frameworks/react/src/App.jsx
@@ -10,11 +10,13 @@ function App() {
   return (
     <div className="min-h-screen bg-slate-950"> {/* min-h-screen = Fond global de la page */}
       <Header />
-      <Hero />
-      <About />
-      <Features />
-      <Insights />
-      <Contact />
+      <main>
+        <Hero />
+        <About />
+        <Features />
+        <Insights />
+        <Contact />
+      </main>
       <Footer />
     </div>
   );
